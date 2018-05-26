326540899bc081833c6fe2837e908a1679a839c5
Soloist <soloist1511@gmail.com>
2018-05-27 01:00:57 +0800

    配合认证拦截，自动获取userId

--- src/main/java/system/attendance/electronic/controller/BaseController.java
+++ src/main/java/system/attendance/electronic/controller/BaseController.java
@@ -1,6 +1,12 @@
 package system.attendance.electronic.controller;
 
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RestController;
+import system.attendance.electronic.common.AuthTokenUtil;
+
+import javax.servlet.http.HttpServletRequest;
 
 /**
  * @author Soloist
@@ -11,4 +17,16 @@ import org.springframework.web.bind.annotation.RestController;
  */
 @RestController
 public abstract class BaseController {
+    
+    protected Long currentUserId;
+    
+    @Autowired
+    protected AuthTokenUtil authTokenUtil;
+    
+    @ModelAttribute
+    private void getCurrentUser(HttpServletRequest request) {
+        String token = request.getHeader("token");
+        currentUserId = authTokenUtil.checkToken(token);
+    }
+    
 }
