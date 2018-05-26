8c41f7dc2aedf593916d6b52e0be88f834860d9e
Soloist <soloist1511@gmail.com>
2018-05-27 00:55:01 +0800

    使用雪花算法获取id

---
+++ src/main/java/system/attendance/electronic/common/SnowFlakeUtil.java
@@ -0,0 +1,24 @@
+package system.attendance.electronic.common;
+
+/**
+ * @author Soloist
+ * @version 1.0
+ * @createtime 2018/5/24 1:27
+ * @email ly@soloist.top
+ * @description
+ */
+public class SnowFlakeUtil {
+
+    private final static long DATACENTER_ID = 1;
+    private final static long MACHINE_ID = 1;
+    private final static SnowFlake SNOW_FLAKE = new SnowFlake(DATACENTER_ID, MACHINE_ID);
+
+    /**
+     * 获取ID
+     * @return ID
+     */
+    public static Long get() {
+        return SNOW_FLAKE.nextId();
+    }
+    
+}
